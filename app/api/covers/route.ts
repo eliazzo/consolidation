e466c35102ccc69e78d7dc434224113d3f8c1a7d
eliazzo <bethazz@hotmail.co.uk>
2023-08-26 16:26:35 +0100

    set up openlibrary api call and render image to books page

---
+++ app/api/covers/route.ts
@@ -0,0 +1,13 @@
+const fetchImage = async (url: string): Promise<HTMLImageElement> => {
+  const img = new Image();
+  return new Promise((res, rej) => {
+      img.onload = () => res(img);
+      img.onerror = e => rej(e);
+      img.src = url;
+
+  });
+}
+
+export default fetchImage
+
+
