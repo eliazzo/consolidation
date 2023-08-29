f141a7890ca4d9750e9631de117adaa2c54305c1
eliazzo <bethazz@hotmail.co.uk>
2023-08-29 19:34:54 +0100

    create image component separate from books page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Bitter } from 'next/font/google'
 import Navbar from './Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        <div className="flex-col items-center h-200 mx-10">
+        <div className="flex-col items-center h-200 mx-10 font-mono">
         {children
         }
         </div>
