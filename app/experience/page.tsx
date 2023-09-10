5a09f5a535268233bbbc9b1a3fa1240921ce2e35
eliazzo <bethazz@hotmail.co.uk>
2023-09-10 14:07:25 +0100

    create experiene page and change navbar colour to green

---
+++ app/experience/page.tsx
@@ -0,0 +1,13 @@
+const Experience = ( ) => { 
+  
+  return (
+    <>  
+    <h1 className="text-center mt-8">Experience</h1>
+    <div className="flex justify-evenly flex-wrap p-8">
+    <p>display previous projects in tiles</p>
+    </div>
+    </>
+  )
+}
+
+export default Experience
