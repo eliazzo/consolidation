189920ba398a3f733d867f9ab7ff3b6285f0b541
eliazzo <bethazz@hotmail.co.uk>
2023-09-11 14:35:45 +0100

    small update to experience

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -4,7 +4,8 @@ const Experience = ( ) => {
     <>  
     <h1 className="text-center mt-8">Experience</h1>
     <div className="flex justify-evenly flex-wrap p-8">
-    <p>display previous projects in tiles</p>
+    <p>display previous projects in tile components</p>
+    <p>Use cinema listings view from cinesphere</p>
     </div>
     </>
   )
