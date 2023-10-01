deab29b1c7c1f23af3557ba87b339eba23b9325c
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-10-01 22:11:13 +0100

    comment next steps

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server"
 
+// create book arr and iterate to render multiple covers
 export const GET = async (req: Request) => {
   const book = "isbn/9780593466322"
   const response = await fetch(`https://covers.openlibrary.org/b/${book}.json`, {
