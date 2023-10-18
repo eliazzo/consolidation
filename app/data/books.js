1a49c4886ba1c228391875471b83418957b91191
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-10-18 15:54:38 +0100

    remove api call and upload images to local storage

---
+++ app/data/books.js
@@ -0,0 +1,9 @@
+// do these need to be stored somewhere else?
+const murakami = "0099448823-L";
+const keegan = "9780802158741";
+const zevin = "9780593321218";
+const salinger = "9780316769532";
+// const caldwell = "isbn/9780571353743";
+// const boyne = "isbn/9781784161002";
+// const hamid = "isbn/9780241953938";
+// const atkinson = "isbn/9780593466322";
