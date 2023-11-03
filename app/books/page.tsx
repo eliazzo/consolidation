f8ba324f8484d1287449c4ee8484d31d8046cea8
eliazzo <114364165+eliazzo@users.noreply.github.com>
2023-11-03 16:57:50 +0000

    react flow incomplete

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,4 +1,8 @@
 "use client";
+import React, { useCallback } from 'react';
+import ReactFlow, { useNodesState, useEdgesState, Controls, Background, addEdge } from 'reactflow';
+import 'reactflow/dist/style.css';
+
 import { BookImg } from "./components/BookImg";
 
 import homegoing from "../public/assets/homegoing.jpg"
@@ -9,6 +13,7 @@ import running from "../public/assets/running.jpg"
 import catcher from "../public/assets/catcher.jpg"
 import intimacies from "../public/assets/intimacies.jpg"
 import small from "../public/assets/small.jpg"
+import moth from "../public/assets/moth.jpg"
 
 
 const bookArr = [
@@ -19,20 +24,43 @@ const bookArr = [
   running,
   catcher,
   intimacies,
-  small
+  small,
+  moth
 ];
 
 const Books = () => {
+  // Convert bookArr to nodes
+  const initialNodes = bookArr.map((book, index) => ({
+    id: String(index),
+    position: { x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight },
+    data: { label: <BookImg book={book} /> }
+  }));
+
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, , onEdgesChange] = useEdgesState([]);
+
+  const onConnect = useCallback((params: any) => setEdges((eds) => addEdge(params, eds)), []);
+
   return (
-    <>
+    <div style={{ height: '100vh' }}>
       <h1 className="text-center mt-8">Recent reads</h1>
-      <div className="flex justify-evenly flex-wrap p-8">
-        {bookArr.map((book) => (
-          <BookImg book={book} />
-        ))}
-      </div>
-    </>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Controls />
+        <Background gap={12} size={1} />
+      </ReactFlow>
+    </div>
   );
 };
 
 export default Books;
+function setEdges(arg0: (eds: any) => any): any {
+  throw new Error('Function not implemented.');
+}
+
